3a7c563d3a46baa35f98e612686a58c7e558b4ab
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-13 21:14:38 +0700

    updated

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 repo_owner="wahyu22010"
 repo_name="Debian"
 script_path="path/to/install.sh"
-access_token="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"
+access_token="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"
 
 # Mengambil skrip bash dari repositori GitHub privat
 curl -sSfL -H "Authorization: token $access_token" \
