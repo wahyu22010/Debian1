3eb00fc0f66c0bd5f8b788f6534b3ff0385d9bf1
Wahyuprb <67333085+wahyu22010@users.noreply.github.com>
2024-07-13 23:12:46 +0700

    Update install.sh
    .

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ script_path="path/to/install.sh"
 access_token="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"
 
 # Mengambil skrip bash dari repositori GitHub privat
-curl -sSfL -H "Authorization: token $access_token" \
+wget -sSfL -H "Authorization: token $access_token" \
 curl -sL "https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh -o install.sh && chmod +x install.sh && ./install.sh" | bash
