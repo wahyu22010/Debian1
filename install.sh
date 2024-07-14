90180ea869d85370bbc6f88929970158c9c5dce2
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-14 15:15:35 +0700

    .

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@
 repo_owner="wahyu22010"
 repo_name="Debian"
 script_path="path/to/install.sh"
-access_token="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"
+access_token="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"
 
 # Mengambil skrip bash dari repositori GitHub privat
 curl -sSfL -H "Authorization: token $access_token" \
-"curl -sL https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh -o install.sh && chmod +x install.sh && ./install.sh" | bash
+    "https://raw.githubusercontent.com/$wahyu22010/$Debian/main/$install.sh" \
+    -o install.sh
+chmod +x install.sh
+./install.sh    
