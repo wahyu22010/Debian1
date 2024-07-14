25841f14a5546a1798598795dd5d8fbe2eb2812f
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-14 15:33:30 +0700

    .

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ access_token="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"
 
 # Mengambil skrip bash dari repositori GitHub privat
 curl -sSfL -H "Authorization: token $access_token" \
-    "https://raw.githubusercontent.com/$wahyu22010/$Debian/main/$install.sh" \
-    -o install.sh
+     "https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh -o install.sh
+
+# Berikan izin eksekusi pada script yang diunduh
 chmod +x install.sh
-./install.sh    
+
+# Jalankan script bash yang diunduh
+./install.sh
