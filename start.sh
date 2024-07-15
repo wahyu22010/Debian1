b63169eec29cf6abddf11f3fff96ad00be603931
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-15 20:14:47 +0700

    .

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Mengambil file raw di github repositori private
 curl -H "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" \
 -H "Accept: application/vnd.github.v3.raw" \
--O https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh
+-O https://raw.githubusercontent.com/wahyu22010/ubuntu/main/install.sh
+ 
+ # Berikan izin eksekusi pada script yang diunduh
+chmod +x install.sh
+
+# Jalankan script bash yang diunduh
+./install.sh
