11cd83f9a269c2a7662f5d1984d036a45425ef7a
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-15 20:05:00 +0700

    new

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -H "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" \
+-H "Accept: application/vnd.github.v3.raw" \
+-O https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh
