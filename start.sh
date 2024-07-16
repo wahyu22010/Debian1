b2af9fa47d0393fb1ac23d827b55ece0cbc63c08
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-16 15:26:55 +0700

    .

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-curl -H "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" \
--H "Accept: application/vnd.github.v3.raw" \
--O https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh 
+# Pengaturan
+repo_owner="wahyu22010"  # Ganti dengan nama pemilik repositori GitHub private
+repo_name="Debian"  # Ganti dengan nama repositori GitHub private
+file_path="install.sh"  # Ganti dengan path file yang ingin diakses
+
+# URL untuk mengambil file raw dari repositori GitHub private
+url="https://raw.githubusercontent.com/$repo_owner/$repo_name/main/$file_path"
+
+# Personal Access Token (PAT)
+access_token="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"  # Ganti dengan personal access token yang valid
+
+# Menggunakan curl dengan header Authorization untuk mengambil file raw
+curl -H "Authorization: token $access_token" -H "Accept: application/vnd.github.v3.raw" -o "$file_path" -L "$url"
+
+#curl -sL https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh?token=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
