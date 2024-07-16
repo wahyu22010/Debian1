529852df4f69d747a5c7850683bb1715d0ecc789
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-16 07:18:53 +0700

    .

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 #Mengambil file raw di github repositori private
 curl -H "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" \
 -H "Accept: application/vnd.github.v3.raw" \
--O https://raw.githubusercontent.com/wahyu22010/ubuntu/main/install.sh && chmod +x install.sh && ./install.sh
+-O https://raw.githubusercontent.com/wahyu22010/Debian/main/install.sh && chmod +x install.sh && ./install.sh
