bda8eee6dfbb1e97ba94256c23f69aa7c78f8dca
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-07-16 07:58:15 +0700

    .

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 repo_owner="wahyu22010"
 repo_name="ubuntu"
 file_path="install.sh"
-access_token="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"  # Ganti dengan token akses yang valid
+access_token="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"  # Ganti dengan token akses yang valid
 
 # URL untuk mengambil file raw dari repositori GitHub privat
 url="https://raw.githubusercontent.com/$repo_owner/$repo_name/main/$file_path"
